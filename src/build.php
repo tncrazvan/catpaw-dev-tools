56c863e8c487af46d903bfff181c8bb675e5dbf4
Razvan Tanase <tangent.jotey@gmail.com>
2022-11-03 02:11:31 +0100

    Version 0.4.46

--- src/build.php
+++ src/build.php
@@ -1,16 +1,18 @@
 <?php
 
 use function Amp\call;
-
+use function Amp\File\createDirectoryRecursively;
 use function Amp\File\deleteFile;
 use function Amp\File\exists;
 use function Amp\File\read;
-
+use function Amp\File\write;
 use Amp\Promise;
+
+use function CatPaw\deleteDirectoryRecursively;
 
 /**
  * 
- * @param string $build name of the build yaml file.
+ * @param string $config name of the build yaml file.
  * 
  * Multiple names separated by "," are allowed, only the first valid name will be used.
  * @return Promise
@@ -19,16 +21,17 @@ function build(
     string $build
 ):Promise {
     return call(function() use ($build) {
-        $config = false;
-        foreach (explode(',', $build) as $file) {
-            if ($parsed = yaml_parse(yield read($file))) {
-                $config = $parsed;
-                break;
+        $buildFileFound = false;
+        foreach (explode(',', $build) as $buildFile) {
+            if (!yield exists($buildFile)) {
+                continue;
             }
+            $buildFileFound = true;
+            $config         = yaml_parse(yield read($buildFile));
         }
-        
-        if (!$config) {
-            die('Invalid yaml build file.'.PHP_EOL);
+
+        if (!$buildFileFound) {
+            die('Build file not found.'.PHP_EOL);
         }
 
         /**
@@ -36,20 +39,22 @@ function build(
          * @var string $entry
          * @var string $libraries
          * @var string $match
-         * @var array  $config
          */
-
-        $name      = $config['name']      ?? '';
+        $name      = $config['name']      ?? 'app.phar';
         $entry     = $config['entry']     ?? '';
         $libraries = $config['libraries'] ?? '';
         $match     = $config['match']     ?? '';
-
+    
         $name      = str_replace(['"',"\n"], ['\\"',''], $name);
         $entry     = str_replace(['"',"\n"], ['\\"',''], $entry);
         $libraries = str_replace(['"',"\n"], ['\\"',''], $libraries);
-    
-        $isWindows = strtoupper(substr(PHP_OS, 0, 3)) === 'WIN';
+        
+        if (!str_ends_with(strtolower($name), '.phar')) {
+            $name .= '.phar';
+        }
 
+        $isWindows = strtoupper(substr(PHP_OS, 0, 3)) === 'WIN';
+    
         if (!str_starts_with($entry, './')) {
             if (!$isWindows) {
                 die("The entry file path must be relative to the project, received: $entry.".PHP_EOL);
@@ -58,7 +63,7 @@ function build(
                 die("The entry file path must be relative to the project, received: $entry.".PHP_EOL);
             }
         }
-    
+        
         foreach (!$libraries?[]:\preg_split('/,|;/', $libraries) as $libraries) {
             if (!str_starts_with($libraries, './')) {
                 if (!$isWindows) {
@@ -69,35 +74,70 @@ function build(
                 }
             }
         }
-
-        $app = str_ends_with(strtolower($name), '.phar')?$name:"$name.phar";
-
+    
+        $app   = "dist/$name";
+        $start = 'dist/start';
+    
+        $dirnameStart = dirname($start);
         try {
+            if (yield exists($dirnameStart)) {
+                yield deleteDirectoryRecursively($dirnameStart);
+            }
+    
+            yield createDirectoryRecursively($dirnameStart);
+    
+            yield write($start, <<<PHP
+                <?php
+                use Amp\File\Filesystem;
+                use Amp\Loop;
+                use CatPaw\Amp\File\CatPawDriver;
+                use CatPaw\Bootstrap;
+                \$_ENV = [
+                    ...\$_ENV,
+                    ...getenv(),
+                ];
+                require 'vendor/autoload.php';
+
+                if (isset(\$_ENV["CATPAW_FILE_DRIVER"]) && \$_ENV["CATPAW_FILE_DRIVER"]) {
+                    Loop::setState(\Amp\File\Driver::class, new Filesystem(new CatPawDriver));
+                }
+                Bootstrap::start(
+                    entry: "$entry",
+                    name: "$name",
+                    libraries: "$libraries",
+                    info: false,
+                    dieOnChange: false,
+                    resources: '',
+                );
+                PHP);
+    
             if (yield exists($app)) {
                 yield deleteFile($app);
             }
-
+    
             if (yield exists($app.'.gz')) {
                 yield deleteFile($app.'.gz');
             }
-        
+            
             $phar = new Phar($app);
-
+    
             $phar->startBuffering();
-
+    
             $phar->buildFromDirectory('.', $match);
-
+    
             $phar->setStub(
-                "#!/usr/bin/env php \n".$phar->createDefaultStub($entry)
+                "#!/usr/bin/env php \n".$phar->createDefaultStub($start)
             );
-
+    
             $phar->stopBuffering();
-
+    
             $phar->compressFiles(Phar::GZ);
-
+    
             # Make the file executable
             chmod($app, 0770);
-
+    
+            yield deleteFile($start);
+    
             echo "$app successfully created".PHP_EOL;
         } catch (Exception $e) {
             die(((string)$e).PHP_EOL);
